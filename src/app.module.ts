4358242eb2f97efb7ccc9ed81f672b7aa3dbe292
Alex <choijeaho86@gmail.com>
2024-01-11 12:25:51 +0900

    파일 명을 포함해서 서버에 파일 올리기

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DownloadFileModule } from './download-file/download-file.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DownloadFileModule],
+  imports: [ConfigModule.forRoot(), DownloadFileModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { UploadService } from './upload.service';
+
+@ApiTags('서버에 파일(이미지 포함) 올리기')
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post('sendToServer')
+  @ApiOperation({
+    summary: '파일을 서버쪽으로 upload',
+    description: '파일을 서버에 올린다. 해당 파일 이름은 한번만 응답된다.',
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          // 사용자가 업로드한 파일의 원래 이름을 사용하여 저장
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const originalName = file.originalname.replace(/\s/g, ''); // 공백 제거
+          const extension = extname(originalName);
+          const fileName = `${originalName}-${uniqueSuffix}${extension}`;
+          callback(null, fileName);
+        },
+      }),
+    }),
+  )
+  downloadFile(@UploadedFile('file') file) {
+    return file;
+  }
+}
